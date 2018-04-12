29b7f0fb300d18af5cea07caefc55bf3b998d64f
doel <adoel.cs@gmail.com>
2018-04-12 17:29:19 +0700

    17.29

--- apps/src/index.js
+++ apps/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// import FilterableProductTable from './FilterableProductTable';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App products={PRODUCTS} />, document.getElementById('root'));
 registerServiceWorker();
